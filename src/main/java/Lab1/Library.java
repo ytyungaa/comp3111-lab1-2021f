42cb11037c1a0df4b019f1422e086ff82b3dd27d
kitty <kitty@localhost>
2021-09-16 23:12:32 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,7 +7,9 @@ public class Library {
 	
 	//function added
 	public static void main(String arg[]) {
-		System.out.println("Hello, World!");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, World!");
 	}
 	
     public boolean someLibraryMethod() {
